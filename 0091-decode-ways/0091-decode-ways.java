ddf8d1feda0649f49172e651cb3097bdcc27122e
Amirtaa-567 <amirtaamp@gmail.com>
2026-09-02 14:00:01 +0530

    Time: 1 ms (80.94%), Space: 43.4 MB (12.92%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDecodings(String s) {
+        if (s.charAt(0) == '0') return 0;
+
+        int prev2 = 1;
+        int prev1 = 1;
+
+        for (int i = 1; i < s.length(); i++) {
+            int cur = 0;
+
+            if (s.charAt(i) != '0')
+                cur += prev1;
+
+            int num = Integer.parseInt(s.substring(i - 1, i + 1));
+
+            if (num >= 10 && num <= 26)
+                cur += prev2;
+
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1;
+    }
+}
